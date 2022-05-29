9fb62bc44752c0c16c88cd50c1d82c0a6881bdd5
Shahid Qureshi <qureshi.shahid7777@gmail.com>
2022-05-29 18:37:52 +0530

    package file modified

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
@@ -6,11 +7,14 @@ var cors = require("cors");
 app.use(express.json({ limit: "10MB" }));
 app.use(cors());
 
-app.use(require("./Route/route"));
-
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("payment-ui/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join("payment-ui/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "payment-ui", "build", "index.html"))
+  );
 }
+
+app.use(require("./Route/route"));
 
 app.listen(PORT, () => {
   console.log(`server running at ${PORT}`);
